5550a8ba926d401c9b928f53ccf309482f035596
Thales Donizetti <TDonizetti@PeopleProductive.com>
2020-03-02 19:12:22 -0300

    understanding GraphQL using /graphQL and in memory array

--- app.js
+++ app.js
@@ -1,12 +1,62 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
+const characters = [];
+
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-    res.send('Hello world!');
-})
+app.use('/graphql', graphqlHttp({
+    schema: buildSchema(`
+        type Character {
+            _id: ID!
+            name: String!
+            isDemonFruitUser: Boolean!
+            isHakiUser: Boolean!
+            age: Int
+            birthDate: String
+        }
+
+        type RootQuery {
+            characters: [Character!]!
+        }
+
+        input characterInput {
+            name: String!
+            isDemonFruitUser: Boolean!
+            isHakiUser: Boolean!
+            age: Int
+            birthDate: String
+        }
+
+        type RootMutation {
+            createCharacter(characterInput: characterInput): Character
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        characters: () => {
+            return characters;
+        },
+        createCharacter: ({ characterInput }) => {
+            const newCharacter = {
+                _id: Math.floor(1000 * Math.random()).toString(),
+                ...characterInput
+            }
+
+            characters.push(newCharacter);
+
+            return newCharacter;
+        }
+    },
+    graphiql: true
+}));
 
 app.listen(3000);
